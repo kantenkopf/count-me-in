3d780fd4695c3f36b9944491f0e1b03c29cb4a8a
Maurice Riebling <m.riebling@outlook.de>
2025-05-06 03:52:16 +0200

    fix(client): missed ternary for deploy env

--- packages/client/src/env/index.ts
+++ packages/client/src/env/index.ts
@@ -1,7 +1,13 @@
+import { ENV_DEPLOY } from "./env.deploy";
 import { ENV_DEV } from "./env.dev";
 import { ENV_PROD } from "./env.prod";
 import { EnvConfig } from "@/types/env.types";
 
-const ENV: EnvConfig = process.env.NODE_ENV === "prod" ? ENV_PROD : ENV_DEV;
+const ENV: EnvConfig =
+  process.env.NODE_ENV === "prod"
+    ? ENV_PROD
+    : process.env.NODE_ENV === "dev"
+    ? ENV_DEV
+    : ENV_DEPLOY;
 
 export default ENV;
